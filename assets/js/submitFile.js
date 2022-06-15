b9f39d292d27bcaa500faab1c3a0e34339c55167
dj-reill <davidjamesreilly19@gmail.com>
2022-06-15 00:28:48 -0400

    submit file update.

--- assets/js/submitFile.js
+++ assets/js/submitFile.js
@@ -1,4 +1,5 @@
 let selectedFile;
+
 function handleFileUploadChange(event){
   selectedFile = event.target.files[0];
 }
@@ -9,21 +10,14 @@ function dismissMessage(event){
 }
 
 function handleFileUploadSubmit(event){
+  const storageBucket = firebase.storage().ref();
   var caption = document.querySelector('#fileCaption');
   var uploadedBy = document.querySelector('#uploader');
   if (uploadedBy.value.length > 0){
-    const storageBucket = firebase.storage().ref();
-    try {
-        var uploadTask = storageBucket.child(`images/${selectedFile.name}`).put(selectedFile);
-    } catch(error) {
-        $('#upload').addClass('form--failure')
-        $('#upload').append('<div class="form_failure"><div class="form_failure_message"> Oh no! Something went wrong! Abandon ship! </div><br><br></div>');
-        $('#upload').append('<div><button class="dismiss primary button">Dismiss</button></div>');
-        console.log(error);
-    }
+    var uploadTask = storageBucket.child(`images/${selectedFile.name}`).put(selectedFile);
 
     uploadTask.on('state_changed', (snapshot) => {
-
+        console.log(snapshot);
     }, (error) => {
         $('#upload').addClass('form--failure')
         $('#upload').append('<div class="form_failure"><div class="form_failure_message"> Oh no! Something went wrong! Abandon ship! </div><br><br></div>');
@@ -31,20 +25,19 @@ function handleFileUploadSubmit(event){
         console.log(error);
     }, () => {
         console.log('file successfully uploaded');
-        
         const data = {'location': `images/${selectedFile.name}`, 'caption': caption.value, 'uploadedBy': uploadedBy.value};
         firebase.database().ref('shared')
             .push()
             .set(data)
-            .then(function(snapshot) {
+            .then(function(s) {
                 $('#upload').addClass('form--success')
-                $('#upload').append('<div class="form_success"><div class="form_success_message"> Thank you for sharing this wonderful day with us</div></div>');
+                $('#upload').append('<div class="form_success"><div class="form_success_message"> Thank you for sharing this wonderful day with us</div><br><br></div>');
                 $('#upload').append('<div><button class="dismiss primary button">Dismiss</button></div>');
                 document.querySelector('.dismiss').addEventListener('click', dismissMessage);
             }, function(error) {
                 console.log('error' + error);
                 //error(); // some error method
-            });   
+            }); 
         });
     }
 }
