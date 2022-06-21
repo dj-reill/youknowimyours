480dc3f0195a6aa201371582b333f3e8ffc71846
dj-reill <davidjamesreilly19@gmail.com>
2022-06-21 17:26:56 -0400

    convert to box view

--- assets/js/loadImages.js
+++ assets/js/loadImages.js
@@ -1,6 +1,7 @@
 const ref = firebase.database().ref('shared');
 const timeline = document.querySelector('#gallery');
 const slices = document.querySelectorAll('[epoch]');
+const type = 'box';
 
 ref.on('child_added', (snapshot, prevChildKey) => {
     const image = snapshot.val();
@@ -11,13 +12,22 @@ ref.on('child_added', (snapshot, prevChildKey) => {
     if (beforeImage.length === 0) {
         beforeImage = [slices[0]];
     }
-    const parentDiv = $(beforeImage[beforeImage.length - 1]);
-    const carousel = parentDiv.find('.carousel')[0];
-    const carouselInner = parentDiv.find('.carousel-inner')[0];
-    appendImage(carouselInner, image);
-    setActiveItem(carouselInner);
-    carousel.closest('.single-timeline-area').removeAttribute('hidden');
-    carousel.removeAttribute("hidden");
+    let parentDiv;
+    let carousel;
+    let carouselInner;
+    if (type === 'box') {
+        parentDiv = $(beforeImage[beforeImage.length - 1]);
+        carousel = parentDiv.find('.box.alt')[0];
+        carouselInner = parentDiv.find('[role="root"]')[0];
+        appendImage(carouselInner, image, type);
+    } else if (type==='carousel'){
+        carousel = parentDiv.find('.carousel')[0];
+        carouselInner = parentDiv.find('.carousel-inner')[0];
+        appendImage(carouselInner, image, type);
+        setActiveItem(carouselInner);
+        carousel.closest('.single-timeline-area').removeAttribute('hidden');
+        carousel.removeAttribute("hidden");
+    }
 });
 
 function setActiveItem(group){
@@ -46,35 +56,59 @@ function isVideo(filename) {
     return false;
   }
 
-function appendImage(target, imageData){
-    // let div = document.createElement('div');
-    // div.className = 'carousel-item';
-    // div.innerHTML = `<img src=${imageData.url} className="img-fluid d-block w-100" alt=${imageData.fileName}><div class="carousel-caption d-none d-md-block"><h5>${imageData.caption}</h5><p>Uploaded By: ${imageData.uploadedBy}</p></div>`;
-    let item = document.createElement('div');
-    item.className = 'carousel-item active';
-    let a = document.createElement('a');
-    a.href = imageData.url;
-    // a.className = `data-toggle="lightbox" data-gallery="${target.id}" data-type="image"`;
-    let img;
-    if(isVideo(imageData.fileName)){
-        img = document.createElement('video');
-    } else {
-        img = document.createElement('img');
-    }
-    img.src = imageData.url;
-    img.className = 'img-fluid d-block w-100';
-    img.alt = imageData.fileName;
-    a.appendChild(img);
-    let caption = document.createElement('div');
-    caption.className = "carousel-caption d-none d-md-block bg-dark mb-4";
-    caption.style = "position: relative; left: 0; top: 0;"
-    let h= document.createElement('h5');
-    h.innerText = imageData.caption;
-    let p = document.createElement('p');
-    p.innerText = `Uploaded by: ${imageData.uploadedBy}`;
-    caption.appendChild(h);
-    caption.appendChild(p)
-    item.appendChild(a);
-    item.appendChild(caption);
-    target.appendChild(item);
+function appendImage(target, imageData, type='carousel'){
+    if (type==='carousel') {
+        // let div = document.createElement('div');
+        // div.className = 'carousel-item';
+        // div.innerHTML = `<img src=${imageData.url} className="img-fluid d-block w-100" alt=${imageData.fileName}><div class="carousel-caption d-none d-md-block"><h5>${imageData.caption}</h5><p>Uploaded By: ${imageData.uploadedBy}</p></div>`;
+        let item = document.createElement('div');
+        item.className = 'carousel-item active';
+        let a = document.createElement('a');
+        a.href = imageData.url;
+        // a.className = `data-toggle="lightbox" data-gallery="${target.id}" data-type="image"`;
+        let img;
+        if(isVideo(imageData.fileName)){
+            img = document.createElement('video');
+        } else {
+            img = document.createElement('img');
+        }
+        img.src = imageData.url;
+        img.className = 'img-fluid d-block w-100';
+        img.alt = imageData.fileName;
+        a.appendChild(img);
+        let caption = document.createElement('div');
+        caption.className = "carousel-caption d-none d-md-block bg-dark mb-4";
+        caption.style = "position: relative; left: 0; top: 0;"
+        let h= document.createElement('h5');
+        h.innerText = imageData.caption;
+        let p = document.createElement('p');
+        p.innerText = `Uploaded by: ${imageData.uploadedBy}`;
+        caption.appendChild(h);
+        caption.appendChild(p)
+        item.appendChild(a);
+        item.appendChild(caption);
+        target.appendChild(item);
+    } else if (type==='box'){
+        let a = document.createElement('a');
+        a.href = imageData.url;
+        // a.className = `data-toggle="lightbox" data-gallery="${target.id}" data-type="image"`;
+        let img;
+        if(isVideo(imageData.fileName)){
+            img = document.createElement('video');
+        } else {
+            img = document.createElement('img');
+        }
+        img.src = imageData.url;
+        img.className = 'img-fluid d-block w-100';
+        img.alt = imageData.fileName;
+        a.appendChild(img);
+        let span = document.createElement('span');
+        span.className = 'image fit';
+        span.id = `image${imageData.size}`;
+        span.appendChild(img);
+        let div = document.createElement('div');
+        div.className = 'col-3';
+        div.appendChild(span);
+        target.appendChild(div);
+    }    
 }
