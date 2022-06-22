a6a17aa9b8937f4b9024d8e19570924037c0bcee
dj-reill <davidjamesreilly19@gmail.com>
2022-06-22 00:16:38 -0400

    better progress bar.

--- assets/js/submitFile.js
+++ assets/js/submitFile.js
@@ -49,14 +49,17 @@ function handleFileUploadSubmit(event){
     if (uploader.length > 0){
         var caption = document.querySelector('#fileCaption');
         // Array of "Promises"
+        const totalBytes = Array.from(selectedFile).map((a) => a.size).reduce((parialSum, a)=> partialSum+a, 0);
         Promise.all(Array.from(selectedFile).map((file, i) => 
-            uploadImageAsPromise(caption.value, uploader, file, i + 1, selectedFile.length)))
+            uploadImageAsPromise(caption.value, uploader, file, i + 1, selectedFile.length, totalBytes)))
         .catch((failure)=>{
             $('#splash').addClass('form--failure')
             $('#splash').append('<div class="form_failure"><div class="form_failure_message"><i class="fa fa-times-circle"></i><p> Oh no! Something went wrong! Abandon ship! </p></div><input type="button" value="Dismiss" class="dismiss primary button"/></div>');
             document.querySelector('.dismiss').addEventListener('click', dismissMessage);
             console.log(error);
         }).then((success)=>{      
+            progressBar.ariaValueNow = 100;
+            progressBar.setAttribute('style',  `width: 100%`);
             $('#splash').addClass('form--success');
             $('#splash').append('<div class="form_success" style="background=#355c78"><div class="form_success_message"> <p style="color: #090d12">Thank you for sharing this wonderful day with us!</p> <input type="button" value="Dismiss" class="button small dismiss"/></div>');
             fileSubmit.setAttribute('disabled', '');
@@ -66,36 +69,27 @@ function handleFileUploadSubmit(event){
 }
 
 //Handle waiting to upload each file using promise
-function uploadImageAsPromise (caption, uploader, selectedFile, fileNumber, total) {
+function uploadImageAsPromise (caption, uploader, selectedFile, fileNumber, total, totalBytes) {
     var progressBar = document.querySelector('[role*=progressbar]');
     var status = document.querySelector('[role*=alert]');
     return new Promise(function (resolve, reject) {
         var storageRef = firebase.storage().ref(`images/${selectedFile.name}`);
-
         //Upload file
         var task = storageRef.put(selectedFile);
-        progressBar.ariaValueNow = String(Number.parseInt(progressBar.ariaValueNow) + 10);
-        progressBar.setAttribute('style',  `width: ${progressBar.ariaValueNow}%`);
 
         //Update progress bar
         task.on('state_changed',
             (snapshot)=> {
-                var percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-                progressBar.ariaValueNow = percentage / total;
+                var percentage = snapshot.bytesTransferred / totalBytes * 100;
+                progressBar.ariaValueNow = percentage;
                 progressBar.setAttribute('style',  `width: ${progressBar.ariaValueNow}%`);
             }, (error) => {
                 console.log(error);
                 return error;
-                // $('#splash').addClass('form--failure')
-                // $('#splash').append('<div class="form_failure"><div class="form_failure_message"><i class="fa fa-times-circle"></i><p> Oh no! Something went wrong! Abandon ship! </p></div><input type="button" value="Dismiss" class="dismiss primary button"/></div>');
-                // document.querySelector('.dismiss').addEventListener('click', dismissMessage);
-                // console.log(error);
             }, () => {
                 firebase.storage().ref().child(`images/${selectedFile.name}`).getDownloadURL().then((url) =>{
                     console.log('file successfully uploaded')
                     status.textContent = `${fileNumber} of ${total} files uploaded`
-                    progressBar.ariaValueNow = String(fileNumber / total * 100);
-                    progressBar.setAttribute('style',  `width: ${progressBar.ariaValueNow}%`);
                     const data = getImageData({url, selectedFile, caption, uploader});
                 
                     firebase.database().ref('shared')
@@ -106,9 +100,6 @@ function uploadImageAsPromise (caption, uploader, selectedFile, fileNumber, tota
                             // do nothing
                         }, function(error) {
                             console.log(error);
-                            // $('#splash').addClass('form--failure')
-                            // $('#splash').append('<div class="form_failure"><div class="form_failure_message"><i class="fa fa-times-circle"></i><p> Oh no! Something went wrong! Abandon ship! </p></div><input type="button" value="Dismiss" class="dismiss primary button"/></div>');        
-                            // document.querySelector('.dismiss').addEventListener('click', dismissMessage);
                         });
                     });
                 return true;
