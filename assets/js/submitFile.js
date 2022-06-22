4044acec069cd349d5503cffd34b5eab31e65467
dj-reill <davidjamesreilly19@gmail.com>
2022-06-21 22:15:23 -0400

    uploadImageAsPromise

--- assets/js/submitFile.js
+++ assets/js/submitFile.js
@@ -33,15 +33,25 @@ function getImageData({url, selectedFile, caption, uploader}){
 
 function handleFileUploadChange(event){
     selectedFile = event.target.files;
+    if (selectedFile.length>1){
+        var status = document.querySelector('[role*=status]');
+        status.removeAttribute('hidden');
+        status.textContent = `${selectedFile.length} files to upload`
+    }
     event.preventDefault();
 }
 
 function handleFileUploadSubmit(event){
     var progress = document.querySelector('[role*=progressbar]');
     var uploadedBy = document.querySelector('#uploader');
+    var status = document.querySelector('[role*=status]');
     const uploader = uploadedBy.value;
     if (uploader.length > 0){
         var caption = document.querySelector('#fileCaption');
+        selectedFile.forEach((file, i) => {
+            uploadImageAsPromise(caption.value, uploader, file);
+            status.textContent = `${i} of ${selectedFile.length} files uploaded`
+        });
         var uploadTask = storageBucket.child(`images/${selectedFile.name}`).put(selectedFile);
         progress.ariaValueNow = String(Number.parseInt(progress.ariaValueNow) + 10);
         progress.setAttribute('style',  `width: ${progress.ariaValueNow}%`);
@@ -82,28 +92,57 @@ function handleFileUploadSubmit(event){
 }
 
 //Handle waiting to upload each file using promise
-function uploadImageAsPromise (selectedFile) {
+function uploadImageAsPromise (caption, uploader, selectedFile) {
+    var progressBar = document.querySelector('[role*=progressbar]');
+    var caption = document.querySelector('#fileCaption');
+
     return new Promise(function (resolve, reject) {
-        var storageRef = firebase.storage().ref(`images/${selectedFile.name});
+        var storageRef = firebase.storage().ref(`images/${selectedFile.name}`);
 
         //Upload file
-        var task = storageRef.put(imageFile);
+        var task = storageRef.put(selectedFile);
+        progressBar.ariaValueNow = String(Number.parseInt(progress.ariaValueNow) + 10);
+        progressBar.setAttribute('style',  `width: ${progress.ariaValueNow}%`);
 
         //Update progress bar
         task.on('state_changed',
             function progress(snapshot){
                 var percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-                uploader.value = percentage;
+                progressBar.ariaValueNow = String(Number.parseInt(percentage) + 10);
+                progressBar.setAttribute('style',  `width: ${percentage}%`);
             },
             function error(err){
-
+                $('#splash').addClass('form--failure')
+                $('#splash').append('<div class="form_failure"><div class="form_failure_message"><p> Oh no! Something went wrong! Abandon ship! </p></div><input type="button" value="Dismiss" class="dismiss primary button"/></div>');
+                document.querySelector('.dismiss').addEventListener('click', dismissMessage);
+                console.log(error);
             },
             function complete(){
-                var downloadURL = task.snapshot.downloadURL;
-            }
-        );
-    });
-}
+                var url = task.snapshot.downloadURL;
+                console.log('file successfully uploaded')
+                progress.ariaValueNow = "100";
+                progress.setAttribute('style',  `width: ${progress.ariaValueNow}%`);
+                const data = getImageData({url, selectedFile, caption, uploader});
+                
+                firebase.database().ref('shared')
+                    .push()
+                    .set(data)
+                    .then(function(s) {
+                        $('#splash').addClass('form--success');
+                        $('#splash').append('<div class="form_success" style="background=#355c78"><div class="form_success_message"> <p style="color: #090d12">Thank you for sharing this wonderful day with us!</p> <input type="button" value="Dismiss" class="button small dismiss"/></div>');
+                        fileSubmit.setAttribute('disabled', '');
+                        document.querySelector('.dismiss').addEventListener('click', dismissMessage);
+                    }, function(error) {
+                        $('#splash').addClass('form--failure')
+                        $('#splash').append('<div class="form_failure"><div class="form_failure_message"><p> Oh no! Something went wrong! Abandon ship! </p></div><input type="button" value="Dismiss" class="dismiss primary button"/></div>');        
+                        document.querySelector('.dismiss').addEventListener('click', dismissMessage);
+                    });
+                }
+            );
+        }
+    );
+};
+
 
 function showModal(event) {
     $('body').removeClass('is-menu-visible');
