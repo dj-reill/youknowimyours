71c7fe49f59dfa91a0abb13c2b73f3e1b910583c
dj-reill <davidjamesreilly19@gmail.com>
2022-06-23 13:23:49 -0400

    identify button

--- assets/js/submitFile.js
+++ assets/js/submitFile.js
@@ -65,26 +65,26 @@ function handleFileUploadSubmit(event){
         })).catch((failure)=>{
             // add alert/warning
             alertBar.classList.add('alert-danger', 'alert-dismissible', 'fade',  'show');
-            const dismiss = $('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+            const dismiss = $('<button id="dismiss-alert" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
             alertBar.appendChild(dismiss[0])
             alertBar.removeAttribute('hidden');
            // $('#splash').addClass('form--failure')
            // $('#splash').append('<div class="form_failure"><div class="form_failure_message"><i class="fa fa-times-circle"></i><p> Oh no! Something went wrong! Abandon ship! </p></div><input type="button" value="Dismiss" class="dismiss primary button"/></div>');
-           alertBar.querySelector('button').addEventListener('click', dismissMessage);
+           alertBar.querySelector('#dismiss-alert').addEventListener('click', dismissMessage);
             console.log(failure);
         }).then((success)=>{      
             progressBar.ariaValueNow = 100;
             progressBar.setAttribute('style',  `width: 100%`);
             progressBar.classList.remove('progress-bar-striped');
             // show success alert.
-            const dismiss = $('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+            const dismiss = $('<button id="dismiss-alert" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
             alertBar.appendChild(dismiss[0])
             alertBar.classList.add('alert-success', 'alert-dismissible', 'fade',  'show');
             alertBar.removeAttribute('hidden');
             // $('#splash').addClass('form--success');
             // $('#splash').append('<div class="form_success" style="background=#355c78"><div class="form_success_message"> <p style="color: #090d12">Thank you for sharing this wonderful day with us!</p> <input type="button" value="Dismiss" class="button small dismiss"/></div>');
             fileSubmit.setAttribute('disabled', '');
-            document.querySelector('button').addEventListener('click', dismissMessage);
+            document.querySelector('#dismiss-alert').addEventListener('click', dismissMessage);
         });
     }
     event.preventDefault();
