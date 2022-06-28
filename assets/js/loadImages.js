5542500e7ca6d3a28a40ea51dad85a354dc51633
dj-reill <davidjamesreilly19@gmail.com>
2022-06-27 21:16:32 -0400

    fix img size with span.

--- assets/js/loadImages.js
+++ assets/js/loadImages.js
@@ -37,19 +37,23 @@ function makeTimelineBucket(image, imageId){
 }
 
 function addToCarousel(image, imageId) {
-  const item = $(`<div class="carousel-item" id="${imageId}">
-                    <div class="row col-12">
-                        <${isVideo(image.fileName) ? 'video': 'img'} src="${image.url}" alt="${image.fileName}" className="img-fluid d-block w-100" style="height: calc( 100vh - 400px )"/>
-                        <div class="carousel-caption d-flex d-block p-2">
-                            <div class="wrapper" style="padding-top:10%">
-                                <h5 style="font-family:'Open Sans', 'Helvetica Neue', Arial, sans-serif; font-weight:400">Uploaded by: ${image.uploadedBy}</h5>
-                                <p>${image.caption}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>`);
-  carousel.querySelector('.carousel-inner').appendChild(item[0]);
-} 
+    const item = $(`<div class="carousel-item" id="${imageId}">
+                      <div class="row col-12">
+                          <span class="image fit" id="${imageId}">
+                              <a href="${image.url}" alt="${image.fileName}" role="click" className="img-fluid"> 
+                                  <${isVideo(image.fileName) ? 'video': 'img'} src="${image.url}" alt="${image.fileName}"/>
+                              </a>
+                          </span>
+                          <div class="carousel-caption d-flex d-block p-2">
+                              <div class="wrapper" style="padding-top:10%">
+                                  <h5 style="font-family:'Open Sans', 'Helvetica Neue', Arial, sans-serif; font-weight:400">Uploaded by: ${image.uploadedBy}</h5>
+                                  <p>${image.caption}</p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>`);
+    carousel.querySelector('.carousel-inner').appendChild(item[0]);
+}
 
 ref.on('child_added', (snapshot, prevChildKey) => {
     makeTimelineBucket(snapshot.val(), snapshot.key );
